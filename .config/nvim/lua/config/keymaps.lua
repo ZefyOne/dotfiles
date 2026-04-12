8d1bce00e71eeaf3d7f124de9dbc153fd7cb6690
zefy <zifeiyu142857@163.com>
2026-04-12 16:15:00 +0800

    更新

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -8,7 +8,6 @@ local keymap = vim.keymap.set
 -- keymap("n", "<C-h>", "^")
 -- keymap("n", "<C-l>", "$")
 
-
 -- i模式 --
 -- move
 keymap("i", "<C-a>", "<esc>^i")
@@ -19,8 +18,10 @@ keymap("i", "<C-l>", "<Right><Backspace>")
 
 keymap("i", "<C-s>", "<esc><cmd>w<cr>a")
 
--- 操作符
+keymap("i", "<Find>", "<esc>^i")
+keymap("i", "<Select>", "<esc>$a")
 
+-- 操作符
 
 -- Yazi
 keymap("n", "<leader>e", function()
@@ -30,17 +31,18 @@ keymap("t", "<leader>e", "q")
 keymap("t", "<esc>", "<C-\\><C-n>")
 
 -- 写文
-keymap("n", "<leader>ta", "<cmd>%s/\\.\\./\\r\\r▲/g|%s/\\./\\r\\r/g|noh<CR>", { desc="格式化整个集纲为正文" })
-keymap("n", "<leader>to", "<cmd>s/ /./g|noh<CR>", { desc="一个空格换成点" })
-keymap("n", "<leader>tn1", "jdjkdiwkko<CR>1-1<CR><CR>日/内<CR><CR>场景：<ESC>po<ESC>", { desc="开头替换" })
-keymap("n", "<leader>tn2", "jdjkdiwkko<CR>1-2<CR><CR>日/内<CR><CR>场景：<ESC>po<ESC>", { desc="开头替换" })
-keymap("n", "<leader>tw1", "jdjkdiwkko<CR>1-1<CR><CR>日/外<CR><CR>场景：<ESC>po<ESC>", { desc="开头替换" })
-keymap("n", "<leader>tw2", "jdjkdiwkko<CR>1-2<CR><CR>日/外<CR><CR>场景：<ESC>po<ESC>", { desc="开头替换" })
+keymap(
+  "n",
+  "<leader>ta",
+  "<cmd>%s/\\.\\./\\r\\r▲/g|%s/\\./\\r\\r/g|noh<CR>",
+  { desc = "格式化整个集纲为正文" }
+)
+keymap("n", "<leader>to", "<cmd>s/ /./g|noh<CR>", { desc = "一个空格换成点" })
+keymap("n", "<leader>tn1", "jdjkdiwkko<CR>1-1<CR><CR>日/内<CR><CR>场景：<ESC>po<ESC>", { desc = "开头替换" })
+keymap("n", "<leader>tn2", "jdjkdiwkko<CR>1-2<CR><CR>日/内<CR><CR>场景：<ESC>po<ESC>", { desc = "开头替换" })
+keymap("n", "<leader>tw1", "jdjkdiwkko<CR>1-1<CR><CR>日/外<CR><CR>场景：<ESC>po<ESC>", { desc = "开头替换" })
+keymap("n", "<leader>tw2", "jdjkdiwkko<CR>1-2<CR><CR>日/外<CR><CR>场景：<ESC>po<ESC>", { desc = "开头替换" })
 
-keymap("n", ",e", "ce（）<ESC>P", { desc="加括号" })
+keymap("n", ",e", "ce（）<ESC>P", { desc = "加括号" })
 -- keymap("v", "<leader>", "<cmd>s/\\%V.*\\%V./（&）/|noh<CR><ESC>") --与上一个功能相同，但开销更大
-keymap("n", ",w", "df）", { desc="删括号" })
-
-
-
-
+keymap("n", ",w", "df）", { desc = "删括号" })
