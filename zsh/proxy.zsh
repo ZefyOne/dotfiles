bfe592010519712fc38149680a164b9afb17d5a5
zefy <zefyHome>
2026-08-14 12:16:21 +0800

    2026-08-14 12:16

--- zsh/proxy.zsh
+++ zsh/proxy.zsh
@@ -1,4 +1,15 @@
-export https_proxy=http://127.0.0.1:7897
-export http_proxy=http://127.0.0.1:7897
-export all_proxy=socks5://127.0.0.1:7897
-export no_proxy=localhost,127.0.0.1
+PROXY_HTTP="http://127.0.0.1:7897"
+PROXY_SOCKS="socks5://127.0.0.1:7897"
+
+proxyon() {
+  export http_proxy="$PROXY_HTTP"
+  export https_proxy="$PROXY_HTTP"
+  export all_proxy="$PROXY_SOCKS"
+  export no_proxy="localhost,127.0.0.1"
+  echo "[proxy] 已开启"
+}
+
+proxyoff() {
+  unset http_proxy https_proxy all_proxy no_proxy
+  echo "[proxy] 已关闭"
+}
